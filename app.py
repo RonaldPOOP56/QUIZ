1d07b70c0b9845cfed687a5669c6349087079218
RonaldPOOP56 <ronaldopoop56@gmail.com>
2026-09-01 22:58:08 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('user.html')
+
+@app.route('/staff')
+def staff():
+    return render_template('/portals/portals.html')
+
+@app.route('/staff/create')
+def create_quiz():
+    return render_template('/portals/create.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
